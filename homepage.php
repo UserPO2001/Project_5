e8705de3ccf401ee77de8ae2e581db01277a0700
UserPO2001 <126059046+UserPO2001@users.noreply.github.com>
2024-10-09 00:48:10 +0200

    Initial commit

---
+++ homepage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wireframe Layout</title>
+    <link rel="stylesheet" href="style.css"> <!-- Linking external CSS file -->
+</head>
+<body>
+
+<div class="centre frame-div">
+    <!-- Welkom *User* DIV -->
+    <div class="Welkom-div">
+        <h1>Welcome, User!</h1>
+    </div>
+
+    <!-- Use DIVS -->
+    <div class="use-parent">
+        <div class="electricitykwh">
+            <div class="icon1"></div>
+        </div>
+        <div class="waterm3">
+            <div class="icon2"></div>
+        </div>
+    </div>
+
+    <!-- Visual use DIVS -->
+    <div class="tips-parent">
+        <div class="tips"></div>
+    </div>
+</div>
+
+</body>
+</html>

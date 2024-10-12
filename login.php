a4e5840589025cbbc8434744d7db6361a8d2f390
UserPO2001 <126059046+UserPO2001@users.noreply.github.com>
2024-10-12 20:15:01 +0200

    edit login

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wireframe Layout</title>
+    <link rel="stylesheet" href="login.css"> <!-- Linking external CSS file -->
+</head>
+<body>
+
+<div class="centre frame-div">
+    <!--  Bedrijfs naam DIV -->
+    <div class="bedrijf-div">
+        <div class="bedrijf-naam">
+            <!-- <h1>Maatschappij</h1> -->
+        </div>
+    </div>
+
+    <!-- login DIVS -->
+    <div class="login-parent">
+        <div class="field-parent">
+            <div class="username"></div>
+        <div class="password"></div>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
